03329aec26927c3dde2577e5582e5a82223141a1
Shivam Garg <shivgarg@live.com>
2019-09-21 01:37:23 -0500

    initial project files

---
+++ project/__init__.py
@@ -0,0 +1,19 @@
+import torch
+    
+def superresolve(img, seed=2019):
+    """
+    YOUR CODE HERE
+    
+    Superresolve an image by a factor of 4
+    @img: A PIL image
+    @return A PIL image
+    
+    e.g:
+    >>> img.size
+    >>> (64,64)
+    >>> img = superresolve(img)
+    >>> img.size
+    >>> (256,256)
+    """
+    
+    raise NotImplemented("superresolve")
